0b765263b1d53be54d5172775976ad293b901a84
usermokhtar <mokhtar.soumair.idrissi@gmail.com>
2024-08-07 16:47:48 +0100

    applied ccs module for p tag in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import logo from '../assets/logo.png';
-import './Header.css';
+import classes from './Header.module.css';
 
 export default function Header() {
   return (
-    <header>
+    <header className={classes.p}>
       <img src={logo} alt='A canvas' />
       <h1>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
